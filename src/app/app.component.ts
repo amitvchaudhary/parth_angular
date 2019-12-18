457d170133c85d60abf64cc4110fad94d28fca90
parth.bhatti.jira <parth.bhatti@localhost.localdomain>
2019-12-18 10:13:02 +0530

    HW

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-first-app';
+  title = 'super 6 - Data';
+  fname = 'Parth';
+  lname = 'B';
+  age = 22;
+  address = 'Ahmedabad';
+  contact = 5252552;
+
+showDetails(){
+ this.showFname();
+ this.showLname();
+ this.showAge();
+ this.showAddress();
+ this.showContact();
 }
+showFname(){
+  console.log(this.fname);
+};
+showLname(){
+  console.log(this.lname);
+};
+showAge(){
+  console.log(this.age);
+};
+showAddress(){
+  console.log(this.address);
+};
+showContact(){
+  console.log(this.contact);
+};
+
+
+}
+
+
